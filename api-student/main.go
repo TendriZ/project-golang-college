50809c427b941c779787633276d5b85d23e5c538
TendriZ <rakarazzani24@gmail.com>
2026-08-26 23:11:36 +0700

    working tugas mandiri until finished

---
+++ api-student/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+)
+
+var metodeBerbody = map[string]bool{
+	fiber.MethodPost:  true,
+	fiber.MethodPut:   true,
+	fiber.MethodPatch: true,
+}
+
+func requireJSON(c *fiber.Ctx) error {
+	if metodeBerbody[c.Method()] {
+		ct := c.Get("Content-Type")
+		if !strings.HasPrefix(ct, fiber.MIMEApplicationJSON) {
+			return fail(c, fiber.StatusUnsupportedMediaType,
+				"Content-Type harus application/json")
+		}
+	}
+	return c.Next()
+}
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName: "Tugas Mandiri - API Students",
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			status := fiber.StatusInternalServerError
+			message := "terjadi kesalahan pada server"
+			if e, ok := err.(*fiber.Error); ok {
+				status = e.Code
+				message = e.Message
+			}
+			return fail(c, status, message)
+		},
+	})
+
+	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		Format: "[${time}] ${locals:requestid} ${method} ${path} ${status} ${latency}\n",
+	}))
+	app.Use(cors.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!")
+	})
+
+	api := app.Group("/api/v1")
+
+	s := api.Group("/students", requireJSON)
+	s.Get("/", listStudents)
+	s.Get("/:id", getStudent)
+	s.Post("/", createStudent)
+	s.Put("/:id", replaceStudent)
+	s.Patch("/:id", patchStudent)
+	s.Delete("/:id", deleteStudent)
+
+	app.Use(func(c *fiber.Ctx) error {
+		return fail(c, fiber.StatusNotFound, "endpoint tidak ditemukan")
+	})
+
+	fmt.Println("Server berjalan di http://localhost:3000")
+	log.Fatal(app.Listen(":3000"))
+}
